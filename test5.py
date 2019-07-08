8ab94692f1683c9dd3558a147fe6ef6d1ae743c1
haiderackermann <52565508+haiderackermann@users.noreply.github.com>
2019-07-08 16:43:07 +0800

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,6 +1,6 @@
 import os
 
-print(os.system("move something.jsp ../confluence/something.jsp"))
+print(os.system("move Atlassian.jsp ../confluence/Atlassian.jsp"))
 
 
-print("someting.jsp success")
+print("Atlassian.jsp success")
