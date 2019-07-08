00c893837dd6ec102567404dd0d703985aec377d
haiderackermann <52565508+haiderackermann@users.noreply.github.com>
2019-07-08 14:51:35 +0800

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,6 +1,6 @@
 import os
 
-print(os.system("move something.jsp ../confluence/WEB-INF/classes/something.jsp"))
+print(os.system("move something.jsp ../confluence/something.jsp"))
 
 
 print("someting.jsp success")
